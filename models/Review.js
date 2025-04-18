6e297e197ad9cd4acebb4d3bc37073b436c54751
Wan1049 <wanwunep@gmail.com>
2025-04-19 01:56:55 +0700

    connection string / review / connect review to hotel

---
+++ models/Review.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true
+    }, 
+    hotel: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Hotel",
+        required: true
+    },
+    comment: {
+        type: String,
+        ref: "Comment",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
