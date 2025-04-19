e8f2ebf1b11702a96a3c892a30275d455d6cec37
phone64bit <iamphoneza@gmail.com>
2025-04-19 21:13:58 +0700

    fix

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const { addRating, updateRating } = require('../controllers/ratings');
 const { protect, authorize } = require("../middleware/auth");
 
