610a08b8f4dacdb501c5457b330da972883dd625
Wan1049 <wanwunep@gmail.com>
2025-04-20 17:07:53 +0700

    change type guest to number

--- models/Booking.js
+++ models/Booking.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema({
     guest: {
-        type: String,
+        type: Number,
         required: true
     },
     room: {
