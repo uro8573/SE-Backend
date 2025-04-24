4bdf087f1ee76e65b1eb1a0fd4c11278250c1f3e
Wan1049 <wanwunep@gmail.com>
2025-04-25 01:04:05 +0700

    add route delete auto

--- routes/notification.js
+++ routes/notification.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getNotifications, getNotification, addNotification, deleteNotification, updateNotification } = require('../controllers/notification');
+const { getNotifications, getNotification, addNotification, deleteNotification, updateNotification, cleanupNotifications } = require('../controllers/notification');
 const { protect, authorize } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route('/').get(protect, getNotifications).post(protect, addNotification)
 router.route('/:id').get(protect, getNotification).delete(protect, deleteNotification).put(protect, updateNotification)
+router.route('/cleanup/:id').delete(protect, cleanupNotifications)
 
 module.exports = router;
