9ca1261cf45031f52825b89a141bcfa44e5f3786
phone64bit <iamphoneza@gmail.com>
2025-04-28 14:02:20 +0700

    Final

--- controllers/config.js
+++ controllers/config.js
@@ -1,5 +1,27 @@
 const Config = require("../models/Config");
 
+//@desc     Get Config
+//@route    PUT /api/v1/config
+//@access   Private
+exports.getConfig = async (req, res, next) => {
+    try {
+        
+        let config = await Config.findOne();
+
+        res.status(200).json({
+            success: true,
+            data: config
+        });
+
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: `Cannot get config with error ${err}.`
+        });
+    }
+}
+
 //@desc     Change Config with specify body
 //@route    PUT /api/v1/config
 //@access   Private
--- routes/config.js
+++ routes/config.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {protect, authorize} = require("../middleware/auth");
-const {updateConfig} = require('../controllers/config');
+const {updateConfig, getConfig} = require('../controllers/config');
 
-router.route("/").put(protect, authorize('admin'), updateConfig);
+router.route("/").get(protect, authorize('admin'), getConfig).put(protect, authorize('admin'), updateConfig);
 
 module.exports = router;
 
